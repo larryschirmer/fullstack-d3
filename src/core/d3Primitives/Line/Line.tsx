f3f6c8e9ef6448da27b353124e253782e959291a
Larry Schirmer <larryschirmer@gmail.com>
2020-05-24 11:15:57 -0400

    add a Line primative

---
+++ src/core/d3Primitives/Line/Line.tsx
@@ -0,0 +1,31 @@
+import React, { FC, useEffect, createRef } from 'react';
+import { select } from 'd3-selection';
+
+type Props = {
+  plot: string;
+  strokeColor: string;
+  strokeWidth: string;
+};
+
+const Line: FC<Props> = ({ plot, strokeColor, strokeWidth }) => {
+  const pathRef = createRef<SVGPathElement>();
+
+  useEffect(() => {
+    if (
+      pathRef !== null &&
+      pathRef !== undefined &&
+      typeof pathRef !== 'function' &&
+      pathRef.current !== null
+    ) {
+      select(pathRef.current)
+        .attr('d', plot)
+        .attr('fill', 'none')
+        .attr('stroke', strokeColor)
+        .attr('stroke-width', strokeWidth);
+    }
+  }, [pathRef, plot, strokeColor, strokeWidth]);
+
+  return <path ref={pathRef} />;
+};
+
+export default Line;
--- src/core/d3Primitives/index.ts
+++ src/core/d3Primitives/index.ts
@@ -1,3 +1,4 @@
 export { default as Chart } from './Chart';
 export { default as Bounds } from './Bounds';
+export { default as Line } from './Line';
 export { default as Rect } from './Rect';
