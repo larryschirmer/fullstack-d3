b97dd4f4da87bad639b69135e836a3d9308e6940
Larry Schirmer <larryschirmer@gmail.com>
2020-05-24 11:15:39 -0400

    add time scale to helpers

--- src/core/d3Helpers/index.ts
+++ src/core/d3Helpers/index.ts
@@ -1,5 +1,6 @@
 export { default as loadRemoteDataset } from './loadRemoteDataset';
 export { default as makeAccessor } from './makeAccessor';
 export { default as makeLinearScale } from './makeLinearScale';
+export { default as makeTimeScale } from './makeTimeScale';
 export { default as processBounds } from './processBounds';
 export { default as useChartDimensions } from './useChartDimensions';
