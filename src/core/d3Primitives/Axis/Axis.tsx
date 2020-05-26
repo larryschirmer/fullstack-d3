a6506691adc8dc64952003f6a224e6686717dc63
Larry Schirmer <larryschirmer@gmail.com>
2020-05-25 21:57:39 -0400

    add support for label and ticks

--- src/core/d3Primitives/Axis/Axis.tsx
+++ src/core/d3Primitives/Axis/Axis.tsx
@@ -8,9 +8,11 @@ import { useChartDimensions } from 'core/d3Helpers';
 type Props = {
   axisType: 'yAxis' | 'xAxis';
   scale: ScaleLinear<number, number> | ScaleTime<number, number>;
+  label?: string;
+  ticks?: number;
 };
 
-const Axis: FC<Props> = ({ axisType, scale }) => {
+const Axis: FC<Props> = ({ axisType, scale, label, ticks }) => {
   const groupRef = createRef<SVGSVGElement>();
   const { dimensions } = useChartDimensions();
 
@@ -29,15 +31,38 @@ const Axis: FC<Props> = ({ axisType, scale }) => {
       const axis = select(groupRef.current);
       const generator = axisTypes[axisType](scale);
 
+      if (ticks) generator.ticks(ticks);
+
       axis.call(generator);
 
       if (axisType === 'xAxis') {
         axis.style('transform', `translateY(${dimensions?.boundedHeight}px)`);
+
+        if (label)
+          axis
+            .append('text')
+            .attr('x', (dimensions?.boundedWidth || 0) / 2)
+            .attr('y', (dimensions?.margin?.bottom || 0) - 10)
+            .attr('fill', 'black')
+            .style('font-size', '1.4em')
+            .html(label);
+      }
+
+      if (axisType === 'yAxis' && label) {
+        axis
+          .append('text')
+          .attr('x', -(dimensions?.boundedHeight || 0) / 2)
+          .attr('y', -(dimensions?.margin?.left || 0) + 10)
+          .attr('fill', 'black')
+          .style('font-size', '1.4em')
+          .html(label)
+          .style('transform', 'rotate(-90deg)')
+          .style('text-anchor', 'middle');
       }
     }
-  }, [axisType, axisTypes, dimensions, groupRef, scale]);
+  }, [axisType, axisTypes, dimensions, groupRef, label, scale, ticks]);
 
-  return <g ref={groupRef}></g>;
+  return <g className="axis" ref={groupRef}></g>;
 };
 
 export default Axis;
