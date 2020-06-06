2c077109197d6b8eae4c77d6f5514fed91e0e8ac
Larry Schirmer <larryschirmer@gmail.com>
2020-06-06 11:52:44 -0400

    add trasition option

--- src/core/d3Primitives/Text/Text.tsx
+++ src/core/d3Primitives/Text/Text.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'react';
+import React, { FC, useRef, useEffect } from 'react';
+import { select } from 'd3-selection';
 
 type Props = {
   x: number;
@@ -6,12 +7,30 @@ type Props = {
   color?: string;
   fontSize?: string;
   className?: string;
+  transition?: boolean;
 };
 
-const Text: FC<Props> = ({ x, y, color = '#000', fontSize = '12px', className, children }) => {
+const Text: FC<Props> = ({
+  x,
+  y,
+  color = '#000',
+  fontSize = '12px',
+  className,
+  transition,
+  children,
+}) => {
+  const textRef = useRef<SVGTextElement>(null!);
+
+  useEffect(() => {
+    const text = transition ? select(textRef.current).transition() : select(textRef.current);
+    
+    text.attr('x', x).attr('y', y);
+  }, [transition, x, y]);
+
   return (
     <text
-      {...{ x, y, className }}
+      ref={textRef}
+      {...{ className }}
       fill={color}
       style={{ fontSize }}
       role="presentation"
