8a5e2e21c100727eb8fd694bbe506d93584ae6da
Larry Schirmer <larryschirmer@gmail.com>
2020-08-02 17:04:32 -0400

    add scales

--- src/YoutubeTutorial/YoutubeTutorial.tsx
+++ src/YoutubeTutorial/YoutubeTutorial.tsx
@@ -1,52 +1,52 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { select, Selection } from 'd3-selection';
+import { scaleLinear, scaleBand } from 'd3-scale';
+import { max } from 'd3-array';
 
 import { Wrapper } from './YoutubeTutorial.styles';
 
 type SelectedSVG = Selection<null, unknown, null, undefined>;
 
 const data = [
-  { units: 150, color: 'purple' },
-  { units: 100, color: 'red' },
-  { units: 50, color: 'blue' },
-  { units: 70, color: 'teal' },
-  { units: 120, color: 'orange' },
+  { name: 'foo', number: 9000 },
+  { name: 'bar', number: 2340 },
+  { name: 'baz', number: 3463 },
+  { name: 'hoge', number: 7654 },
+  { name: 'piyo', number: 8746 },
 ];
+
+const svgSize = { width: 800, height: 500 };
 
 const YoutubeTutorial = () => {
   const svgRef = useRef(null);
   const [selection, setSelection] = useState<SelectedSVG | null>(null);
 
+  const x = scaleBand()
+    .domain(data.map(({ name }) => name))
+    .range([0, svgSize.width])
+    .paddingInner(0.05)
+    .paddingOuter(0.7);
+  const y = scaleLinear()
+    .domain([0, max(data, ({ number }) => number) ?? 0])
+    .range([0, svgSize.height]);
+
   useEffect(() => {
-    if (!selection) {
-      setSelection(select(svgRef.current));
-      return;
-    }
-
-    const rects = selection
-      .selectAll('rect')
-      .data(data) // for each over the selection, note elements that do not fit
-      .attr('width', (d) => 100)
-      .attr('height', (d) => d.units)
-      .attr('fill', (d) => d.color)
-      .attr('x', (_, i) => i * 100);
-
-    rects
-      .enter()
-      .append('rect')
-      .attr('width', (d) => 100)
-      .attr('height', (d) => d.units)
-      .attr('fill', (d) => d.color)
-      .attr('x', (_, i) => i * 100);
-  }, [selection]);
+    if (!selection) setSelection(select(svgRef.current));
+    else
+      selection
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('width', x.bandwidth)
+        .attr('height', ({ number }) => y(number))
+        .attr('x', ({ name }) => x(name) ?? null)
+        .attr('fill', 'orange');
+  }, [selection, x, y]);
 
   return (
     <Wrapper>
-      <svg ref={svgRef} width={500}>
-        <rect />
-        <rect />
-        <rect />
-      </svg>
+      <svg ref={svgRef} width={svgSize.width} height={svgSize.height} />
     </Wrapper>
   );
 };
